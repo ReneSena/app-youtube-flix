95840b4a1e39d6e118c2d5e0836ffd875274fe1d
ReneSena <rene.sena3005@gmail.com>
2021-04-20 23:28:48 -0300

    feat(src/routes): routes structure to manage pages on app

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import HomePage from '../pages/home';
+import WatchPage from '../pages/watch';
+import FavoritePage from '../pages/favorite';
+import SearchPage from '../pages/search';
+import Page404 from '../pages/404';
+
+export default function Routes() {
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" exact component={HomePage} />
+				<Route path="/watch" component={WatchPage} />
+				<Route path="/favorites" component={FavoritePage} />
+				<Route path="/search" component={SearchPage} />
+				<Route path="*" component={Page404} />
+			</Switch>
+		</BrowserRouter>
+	);
+}
