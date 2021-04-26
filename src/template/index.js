d8cffe214f527f82df2f11da21bf75232289764d
= <=>
2021-04-26 00:28:41 -0300

    refactor(template): refactory styles in template pages

--- src/template/index.js
+++ src/template/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-
-const Box = styled.main`
-	margin: 100px 20px 40px 20px;
-
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	flex-direction: column;
-`;
+import { Main } from './styles';
 
 function template({ children }) {
 	return (
 		<>
 			<Header />
-			<Box>{children}</Box>
+			<Main>{children}</Main>
 			<Footer />
 		</>
 	);
---
+++ src/template/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+	margin: 100px 20px 40px 20px;
+
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+`;
